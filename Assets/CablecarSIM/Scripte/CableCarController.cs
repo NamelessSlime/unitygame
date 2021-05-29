4d3144b789f8407178565923ba69e3dd9d05ea7f
NamelessSlime <80654502+NamelessSlime@users.noreply.github.com>
2021-05-29 16:57:57 +0900

    5-29

--- Assets/CablecarSIM/Scripte/CableCarController.cs
+++ Assets/CablecarSIM/Scripte/CableCarController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CableCarController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rigidbody2d;
+
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.D))
+        {
+            rigidbody2d.AddForce(new Vector2(50.0f, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.A))
+        {
+            rigidbody2d.AddForce(new Vector2(-50.0f, 0));
+        }
+
+
     }
 }
